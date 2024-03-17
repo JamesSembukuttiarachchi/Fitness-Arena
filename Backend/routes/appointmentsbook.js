2851a5b234a3e400220057f7daf999e5160ac933
fernandoMHSJ <it22086402@my.sliit.lk>
2024-03-17 11:30:28 +0530

    upload to hiruni

---
+++ Backend/routes/appointmentsbook.js
@@ -0,0 +1,110 @@
+const router = require("express").Router();
+let appointments =require("../models/appointment");
+
+//create
+
+router.route("/create").post((req,res)=>{
+
+const userid = req.body.userid;
+const firstname = req.body.firstname;
+const lastname = req.body.lastname;
+const trainername = req.body.trainername;
+const date = Date(req.body.date);
+const time = req.body.time;
+const email = req.body.email;
+const phone = Number(req.phone.phone);
+
+const newappointment = new appointments({
+
+     userid, 
+     firstname, 
+     lastname, 
+     trainername, 
+     date, 
+     time,
+     email, 
+     phone 
+})
+
+
+new appointments.save().then(()=>{
+   res.json("Appointment Booked")
+}).catch((err)=>{
+       console.log(err);
+
+})
+
+})
+
+//view
+
+router.route("/").get((req,res)=>{
+
+
+appointments.save().then((appo)=>{
+     res.json(appo)
+
+}).catch((err)=>{
+    console.log(err)
+})
+
+})
+
+//update
+
+router.route("/update/:id").put(async(req,res)=>{
+let uid = req.params.id;
+const userid = req.body.userid;
+const firstname = req.body.firstname;
+const lastname = req.body.lastname;
+const trainername = req.body.trainername;
+const date = Date(req.body.date);
+const time = req.body.time;
+const email = req.body.email;
+const phone = Number(req.phone.phone);
+
+const updateappo = {
+   userid, 
+   firstname, 
+   lastname, 
+   trainername, 
+   date, 
+   time,
+   email, 
+   phone 
+
+}
+const update = await appointment.findOneAndUpdate(userid,updateappo)
+.then(()=>{
+   res.status(200).send({status:"appointment updated",user:update})
+}).catch((err)=>{
+
+ console.log(err);
+ res.status(500).send({status:"update Error"});
+
+})
+
+})
+
+//delete
+
+
+router.route("/delete/:id").delete(async(req,res)=>{
+
+let userid = req.params.userid;
+await appointment.findOneAndDelete(userid)
+.then(()=>{
+   res.status(200).send({status:"appointment deleted",user:update})
+}).catch((err)=>{
+
+ console.log(err);
+ res.status(500).send({status:"delete Error"});
+
+})
+
+
+
+})
+
+module.exports = router;
+
