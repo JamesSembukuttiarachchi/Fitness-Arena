cb07539267cfab815e322d8acb1ab15a083dcfdf
fernandoMHSJ <127774283+fernandoMHSJ@users.noreply.github.com>
2024-04-04 22:22:02 +0530

    Add files via upload

---
+++ Backend/appointment.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+const appointmentSchema = mongoose.Schema(
+  {
+    userid: {
+      type: String,
+      required: true,
+    },
+    firstname: {
+        type: String,
+        required: true,
+      },
+      lastname: {
+        type: String,
+        required: true,
+      },
+      trainername: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        required: true,
+      },
+      time: {
+        type: String,
+        required: true,
+      },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+  },
+
+);
+
+const appoinment = mongoose.model("appointment", appointmentSchema);
