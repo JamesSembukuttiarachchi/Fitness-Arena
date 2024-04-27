d6fa9e008d2f1e8a88d9d83a3c88e07dabb5ba97
minidufonseka <minidufonseka@gmail.com>
2024-04-27 11:23:36 +0530

    Commit

--- Frontend/src/pages/SaveCard.jsx
+++ Frontend/src/pages/SaveCard.jsx
@@ -164,34 +164,16 @@ const SaveCard = () => {
 
                 <div className="flex flex-col">
                   <label>Expiry Month: </label>
-                  <select
-                    name="expiry-month"
-                    id="expiry-month"
-                    class="appearance-none w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
-                  >
-                    <option value={""}>Please Select</option>
-                    <option value={"1"}>01</option>
-                    <option value={"2"}>02</option>
-                    <option value={"3"}>03</option>
-                    <option value={"4"}>04</option>
-                    <option value={"5"}>05</option>
-                    <option value={"6"}>06</option>
-                    <option value={"7"}>07</option>
-                    <option value={"8"}>08</option>
-                    <option value={"9"}>09</option>
-                    <option value={"10"}>10</option>
-                    <option value={"11"}>11</option>
-                    <option value={"12"}>12</option>
-                  </select>
+                  
                   {/* md */}
-                  {/* <input
+                  <input
                     name="expiryMonth"
                     value={form2.expiryMonth}
                     onChange={handleChangeForm2}
                     type="number"
                     placeholder="Type here"
                     className="input input-bordered input-md w-full max-w-xs"
-                  /> */}
+                  />
                   <ErrorMessage
                     name="expiryMonth"
                     component="div"
@@ -201,30 +183,15 @@ const SaveCard = () => {
                 <div className="flex gap-1">
                   <div className="flex flex-col">
                     <label>Expiry Year: </label>
-
-                    <select
-                      name="expiry-year"
-                      id="expiry-year"
-                      class="appearance-none w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
-                    >
-                      <option value={""}>Please Select</option>
-                      <option value={"2024"}>2024</option>
-                      <option value={"2025"}>2025</option>
-                      <option value={"2026"}>2026</option>
-                      <option value={"2027"}>2027</option>
-                      <option value={"2028"}>2028</option>
-                      <option value={"2029"}>2029</option>
-                      <option value={"2030"}>2030</option>
-                    </select>
                     {/* md */}
-                    {/* <input
+                    <input
                       name="expiryYear"
                       value={form2.expiryYear}
                       onChange={handleChangeForm2}
                       type="number"
                       placeholder="Type here"
                       className="input input-bordered input-md w-full max-w-xs"
-                    /> */}
+                    />
                     <ErrorMessage
                       name="expiryYear"
                       component="div"
