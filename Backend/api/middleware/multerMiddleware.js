7fc8edbc5496f8cc22774b5049fcd8c4099c239f
RakinduM <rakindumarambe17@gmail.com>
2024-05-12 05:33:16 +0530

    :)

--- Backend/api/middleware/multerMiddleware.js
+++ Backend/api/middleware/multerMiddleware.js
@@ -1,13 +1,14 @@
-import multer, { diskStorage } from "multer";
+import multer, { diskStorage } from 'multer';
+
 
 // Set storage engine
 const storage = diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/offers/"); // Specify the directory where files will be uploaded
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname); // Specify the file naming scheme
-  },
+    destination: function (req, file, cb) {
+        cb(null, 'public/offers/'); // Specify the directory where files will be uploaded
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname); // Specify the file naming scheme
+    }
 });
 
 // Initialize upload
