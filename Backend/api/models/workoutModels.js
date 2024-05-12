3a024b59fa8d0b70614cb8d705b0d24f9bf4b6e1
Rakindu Marambe <116582517+RakinduM@users.noreply.github.com>
2024-05-12 20:44:20 +0530

    Create workoutModels.js

---
+++ Backend/api/models/workoutModels.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const workoutSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Reference to the User model
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const workout = mongoose.model("Workout", workoutSchema);
